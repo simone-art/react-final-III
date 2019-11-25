f40f2bec702d14314c2ae19d6e3e12e68d24a83b
simone-art <simoneperiodista3@gmail.com>
2019-11-25 15:19:49 -0300

    creado com react, un button para contar sin click

---
+++ my-contador/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from './componentes/Button'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Contador</h2>
+      <Button />
+      
+    </div>
+  );
+}
+
+export default App;
